4a0672beedaa90463ae6cc30281a61e1e9eb6b2b
Abhishek Kumar <kumarabhi7480>
2023-04-28 21:54:38 +0530

    dashboard page added

--- src/pages/Dashboard/components/DashboardCard.js
+++ src/pages/Dashboard/components/DashboardCard.js
@@ -1,4 +1,4 @@
-export const DashbaordCard = () => {
+export const DashboardCard = () => {
     return (
       <div className="max-w-4xl m-auto p-2 mb-5 border dark:border-slate-700">
           <div className="flex justify-between text-sm m-2 font-bold dark:text-slate-200">
