61f4cf2dd27f336cc9dc3d10ed32f5939ea57f85
Abhishek Kumar <kumarabhi7480>
2023-04-30 23:29:55 +0530

    update login guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,8 +25,8 @@ export const Login = () => {
   }
   
   async function handleLoginGuest(){
-    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
-    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
+    email.current.value = "abhishek@kumar.com";
+    password.current.value = "Abhi123";
     try{
       const authDetail = {
         email: email.current.value,
