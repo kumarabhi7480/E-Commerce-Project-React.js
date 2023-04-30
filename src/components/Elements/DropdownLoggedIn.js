1459f14375f09601e5187e0e3f7e48f5ddba9af4
Abhishek Kumar <kumarabhi7480>
2023-04-30 14:39:00 +0530

    update

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+
 import { getUser, logout } from "../../services";
 
 export const DropdownLoggedIn = ({setDropdown}) => {
